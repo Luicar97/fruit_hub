2f48fe085399a7fd843314c97fac1d5663bf41d2
Carlos Francisco <carlos@tutati.com>
2021-06-23 17:22:54 -0500

    cambios

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -9,7 +9,54 @@ class HomePage extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            Text('data'),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 16,
+                right: 16,
+                top: 30,
+              ),
+              child: Flex(
+                direction: Axis.horizontal,
+                children: [
+                  Container(
+                    child: Icon(
+                      Icons.short_text,
+                      size: 35,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.only(
+                        left: 10,
+                      ),
+                      child: Text(
+                        'Bienvenido, Carlos Mendoza Garay',
+                        style: TextStyle(
+                          fontFamily: 'TTNorms',
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        //color: Colors.red,
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(
+                          color: Colors.grey.withOpacity(0.1),
+                        )),
+                    child: Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: Image.asset(
+                        'assets/img/canasta2.png',
+                        width: 20,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
