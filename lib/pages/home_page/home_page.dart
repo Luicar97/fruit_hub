5b0a6149a26492fde3c3829756d47697f5cd389a
Carlos Francisco <carlos@tutati.com>
2021-06-24 13:30:54 -0500

    cambios

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -3,6 +3,7 @@ import 'package:fruit_hub/pages/home_page/widgets/carrousel_option_widget.dart';
 import 'package:fruit_hub/pages/home_page/widgets/head_home_widget.dart';
 import 'package:fruit_hub/widgets/line_widget.dart';
 import 'package:fruit_hub/widgets/title_regular_widget.dart';
+import 'package:fruit_hub/widgets/title_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -47,7 +48,7 @@ class HomePage extends StatelessWidget {
                           prefixIcon: Icon(
                             Icons.search,
                           ),
-                          hintText: 'Buscar ensaladas de frutas ',
+                          hintText: 'Buscar ensaladas de frutas',
                           hintStyle: TextStyle(
                             color: Colors.grey.withOpacity(
                               0.4,
@@ -87,37 +88,293 @@ class HomePage extends StatelessWidget {
               ),
             ),
             CarruselOptionWidget(),
-            SizedBox(
-              height: 15,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                right: 16,
-                left: 16,
-              ),
-              child: TitleRegularWidget(
-                title: 'Combo recomendado',
-              ),
-            ),
-            LineWidget(),
-            Flex(
-              direction: Axis.horizontal,
-              children: [
-                Expanded(
-                  child: Container(
-                      color: Colors.red,
-                      child: Column(
-                        children: [
-                          Container(
-                            child: Image.asset('assets/img/comida1.png'),
+            Container(
+              color: Color.fromRGBO(229, 229, 229, 0.3),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      right: 16,
+                      left: 16,
+                    ),
+                    child: TitleRegularWidget(
+                      title: 'Combo recomendado',
+                    ),
+                  ),
+                  LineWidget(),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 16,
+                      left: 16,
+                      right: 16,
+                    ),
+                    child: Flex(
+                      direction: Axis.horizontal,
+                      children: [
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white,
+                            ),
+
+                            padding: EdgeInsets.only(
+                              left: 8,
+                              right: 8,
+                            ),
+                            //color: Colors.red,
+                            child: Column(
+                              children: [
+                                Stack(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.all(5.0),
+                                      child: Align(
+                                        alignment: Alignment.centerRight,
+                                        child: Container(
+                                          //color: Colors.amber,
+                                          child: Icon(
+                                            Icons.favorite_border_rounded,
+                                            color: Color.fromRGBO(
+                                              255,
+                                              164,
+                                              81,
+                                              1,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                        top: 16,
+                                      ),
+                                      child: Container(
+                                        alignment: Alignment.center,
+                                        child: Image.asset(
+                                            'assets/img/comida1.png'),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Container(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    'Honey lime combo',
+                                    style: TextStyle(
+                                      fontFamily: 'TTNorms-Bold',
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                    top: 5,
+                                    bottom: 12,
+                                  ),
+                                  child: Flex(
+                                    direction: Axis.horizontal,
+                                    children: [
+                                      Expanded(
+                                        child: Container(
+                                          height: 25,
+                                          //color: Colors.amber,
+                                          alignment: Alignment.bottomLeft,
+                                          child: Text(
+                                            'S/2,000',
+                                            style: TextStyle(
+                                              fontFamily: 'TTNorms-Bold',
+                                              fontSize: 15,
+                                              color: Color.fromRGBO(
+                                                255,
+                                                164,
+                                                81,
+                                                1,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child: Container(
+                                          padding: EdgeInsets.only(
+                                            right: 5,
+                                          ),
+                                          alignment: Alignment.centerRight,
+                                          //color: Colors.red,
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                                color: Color.fromRGBO(
+                                                  255,
+                                                  242,
+                                                  231,
+                                                  1,
+                                                ),
+                                                borderRadius:
+                                                    BorderRadius.circular(
+                                                  10,
+                                                )),
+                                            child: Icon(
+                                              Icons.add,
+                                              color: Color.fromRGBO(
+                                                255,
+                                                164,
+                                                81,
+                                                1,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
-                        ],
-                      )),
-                ),
-                SizedBox(
-                  width: 20,
-                ),
-              ],
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white,
+                            ),
+
+                            padding: EdgeInsets.only(
+                              left: 8,
+                              right: 8,
+                            ),
+                            //color: Colors.red,
+                            child: Column(
+                              children: [
+                                Stack(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.all(5.0),
+                                      child: Align(
+                                        alignment: Alignment.centerRight,
+                                        child: Container(
+                                          //color: Colors.amber,
+                                          child: Icon(
+                                            Icons.favorite_border_rounded,
+                                            color: Color.fromRGBO(
+                                              255,
+                                              164,
+                                              81,
+                                              1,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                        top: 16,
+                                      ),
+                                      child: Container(
+                                        alignment: Alignment.center,
+                                        child: Image.asset(
+                                            'assets/img/comida2.png'),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Container(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    'Mango combo',
+                                    style: TextStyle(
+                                      fontFamily: 'TTNorms-Bold',
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                    top: 5,
+                                    bottom: 12,
+                                  ),
+                                  child: Flex(
+                                    direction: Axis.horizontal,
+                                    children: [
+                                      Expanded(
+                                        child: Container(
+                                          height: 25,
+                                          //color: Colors.amber,
+                                          alignment: Alignment.bottomLeft,
+                                          child: Text(
+                                            'S/2,000',
+                                            style: TextStyle(
+                                              fontFamily: 'TTNorms-Bold',
+                                              fontSize: 15,
+                                              color: Color.fromRGBO(
+                                                255,
+                                                164,
+                                                81,
+                                                1,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child: Container(
+                                          padding: EdgeInsets.only(
+                                            right: 5,
+                                          ),
+                                          alignment: Alignment.centerRight,
+                                          //color: Colors.red,
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                                color: Color.fromRGBO(
+                                                  255,
+                                                  242,
+                                                  231,
+                                                  1,
+                                                ),
+                                                borderRadius:
+                                                    BorderRadius.circular(
+                                                  10,
+                                                )),
+                                            child: Icon(
+                                              Icons.add,
+                                              color: Color.fromRGBO(
+                                                255,
+                                                164,
+                                                81,
+                                                1,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                ],
+              ),
             ),
           ],
         ),
