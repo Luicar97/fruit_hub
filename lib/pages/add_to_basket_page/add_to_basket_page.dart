3cbf3ef58b46f2fa0e45c86987d3c3bc0247b04f
Carlos Francisco <carlos@tutati.com>
2021-06-24 17:37:54 -0500

    homescreem

---
+++ lib/pages/add_to_basket_page/add_to_basket_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddToBasketPage extends StatelessWidget {
+  const AddToBasketPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [
+          Text('data'),
+        ],
+      )),
+    );
+  }
+}
