2a5455f8a3364a63629f86e9dd1bb8c50c36e6dc
Carlos Francisco <carlos@tutati.com>
2021-06-24 19:49:19 -0500

    cambio

--- lib/pages/add_to_basket_page/add_to_basket_page.dart
+++ lib/pages/add_to_basket_page/add_to_basket_page.dart
@@ -6,10 +6,52 @@ class AddToBasketPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(
+        255,
+        164,
+        81,
+        1,
+      ),
       body: SafeArea(
           child: Column(
         children: [
-          Text('data'),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 30,
+              left: 20,
+            ),
+            child: Container(
+              width: 75,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: Colors.white,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Row(
+                  children: [
+                    Container(
+                      child: Icon(
+                        Icons.arrow_back_ios_outlined,
+                      ),
+                    ),
+                    Container(
+                      child: Text(
+                        'Atras',
+                        style: TextStyle(
+                          fontFamily: 'TTNorms',
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: Image.asset('assets/img/comidagrande.png'),
+          )
         ],
       )),
     );
