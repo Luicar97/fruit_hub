1dc4b6f39ae7c806bfaf9f9b0ede080315827d0c
Carlos Francisco <carlos@tutati.com>
2021-06-25 23:09:47 -0500

    widgets

---
+++ lib/pages/order_list_page/order_list_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OrderListPage extends StatelessWidget {
+  const OrderListPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      )),
+    );
+  }
+}
